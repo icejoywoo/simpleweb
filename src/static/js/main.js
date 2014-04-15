a108779c9735603af2007383629d73654e17d7f3
icejoywoo <icejoywoo@gmail.com>
2014-04-15 23:21:10 +0800

    update

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -7,3 +7,19 @@ var app = angular.module('myApp', []);
 app.run(function ($rootScope) {
     $rootScope.name = "icejoywoo";
 });
+
+app.controller('MyController', function($scope) {
+    $scope.person = {
+        name: "icejoywoo"
+    }
+});
+
+app.controller('ParentController', function($scope) {
+  $scope.person = {greeted: false};
+});
+
+app.controller('ChildController', function($scope) {
+  $scope.sayHello = function() {
+    $scope.person.greeted = true;
+  }
+});
